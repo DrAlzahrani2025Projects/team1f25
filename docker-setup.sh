ac24581d0773ed49b87071514d76c5ac387684d4
008993368-yaz <008993368@coyote.csusb.edu>
2025-09-18 23:36:36 -0700

    fix docker-setup.sh (#8)

--- docker-setup.sh
+++ docker-setup.sh
@@ -3,10 +3,12 @@ set -euo pipefail
 
 if [ "$#" -eq 1 ] && [ "$1" = "local" ]; then
   echo "--- Local deployment ---"
-  ./build-depoly-app.sh
+  chmod +x build-deploy-app.sh
+  ./build-deploy-app.sh
   echo "✅ Local app available at: http://localhost:5001/team1f25/"
 else
   echo "--- Remote deployment ---"
+  chmod +x build-deploy-app.sh apache-setup.sh
   ./build-deploy-app.sh
   sudo ./apache-setup.sh
   echo "✅ Remote app available at: https://sec.cse.csusb.edu/team1f25/"
