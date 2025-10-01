cad3adc4c634928a2b8d847381e822063e9b323a
Yazhini Elanchezhian <008993368@coyote.csusb.edu>
2025-09-30 20:38:05 -0700

    Update docker-setup.sh to remove sudo usage (#11)
    * Update docker-setup.sh to remove sudo usage
    
    Removed sudo from script commands and adjusted permissions.
    
    * Modify ServerName and clean up proxy configuration
    
    Updated ServerName and removed unnecessary proxy settings.
    
    * Change greeting message in app.py

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-sudo chmod +x build-deploy-app.sh apache-setup.sh docker-cleanup.sh
-sudo ./build-deploy-app.sh
-sudo ./apache-setup.sh
-echo "✅ Remote app available at: https://sec.cse.csusb.edu/team1f25/"
+chmod +x build-deploy-app.sh docker-cleanup.sh
+./build-deploy-app.sh
+#sudo ./apache-setup.sh
